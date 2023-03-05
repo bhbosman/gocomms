c0ba578e664687f131b46fe5283b8a40794823f7
Brendan Bosman <bhbosman@gmail.com>
2023-03-05 22:04:10 +0200

    refactoring

--- common/twoWayPipeDefinition_test.go
+++ common/twoWayPipeDefinition_test.go
@@ -2,19 +2,18 @@ package common_test
 
 import (
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/fx/fxtest"
 	"testing"
 )
 
-
-
-
-
-func CreateOneWayPipe[source any, destination any]() {
-
-}
-
 func TestTwoWayPipeDefinition(t *testing.T) {
-
 	ctrl := gomock.NewController(t)
+
+	app := fxtest.New(t)
+	assert.NoError(t, app.Err())
+	app.RequireStart()
+
+	app.RequireStop()
 	defer ctrl.Finish()
 }
