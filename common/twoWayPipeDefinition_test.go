ceebeb78de4dadcc2c31b3d26c2c602557eea96d
Brendan Bosman <bhbosman@gmail.com>
2023-03-05 21:39:11 +0200

    refactoring

--- common/twoWayPipeDefinition_test.go
+++ common/twoWayPipeDefinition_test.go
@@ -1,9 +1,20 @@
 package common_test
 
 import (
+	"github.com/golang/mock/gomock"
 	"testing"
 )
 
+
+
+
+
+func CreateOneWayPipe[source any, destination any]() {
+
+}
+
 func TestTwoWayPipeDefinition(t *testing.T) {
-	//t := common.NewTwoWayPipeDefinition()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 }
