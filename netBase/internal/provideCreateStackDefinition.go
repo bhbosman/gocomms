3130328d22bd81a2826527bf07cafed7067b9ff4
Brendan Bosman <bhbosman@gmail.com>
2023-03-06 09:28:47 +0200

    refactoring

--- netBase/internal/provideCreateStackDefinition.go
+++ netBase/internal/provideCreateStackDefinition.go
@@ -12,102 +12,112 @@ import (
 	"strings"
 )
 
+type CreateStackDefinition struct {
+	fx.Out
+	StackState             []common.IStackState
+	InboundPipeDefinition  common.IInboundPipeDefinition
+	OutboundPipeDefinition common.IOutboundPipeDefinition
+}
+
 func ProvideCreateStackDefinition() fx.Option {
 	return fx.Provide(
-		fx.Annotated{
-			Target: func(
-				params struct {
-					fx.In
-					CancelFunc           context.CancelFunc
-					ConnectionCancelFunc model.ConnectionCancelFunc
-					Logger               *zap.Logger
-					StackName            string                                 `name:"StackName"`
-					TransportFactories   []*goCommsDefinitions.TransportFactory `group:"TransportFactory"`
-					StackFactories       []common.IStackDefinition              `group:"StackDefinition"`
-				},
-			) ([]common.IStackState, common.IInboundPipeDefinition, common.IOutboundPipeDefinition, error) {
-				params.Logger.Info("createStackDefinition...")
-				var factory *goCommsDefinitions.TransportFactory = nil
-				for _, item := range params.TransportFactories {
-					if item.Name == params.StackName {
-						factory = item
-						break
+
+		func(
+			params struct {
+				fx.In
+				CancelFunc           context.CancelFunc
+				ConnectionCancelFunc model.ConnectionCancelFunc
+				Logger               *zap.Logger
+				StackName            string                                 `name:"StackName"`
+				TransportFactories   []*goCommsDefinitions.TransportFactory `group:"TransportFactory"`
+				StackFactories       []common.IStackDefinition              `group:"StackDefinition"`
+			},
+		) (CreateStackDefinition, error) {
+			params.Logger.Info("createStackDefinition...")
+			var factory *goCommsDefinitions.TransportFactory = nil
+			for _, item := range params.TransportFactories {
+				if item.Name == params.StackName {
+					factory = item
+					break
+				}
+			}
+			if factory != nil {
+				dict := make(map[string]common.IStackDefinition)
+				for _, stackFactory := range params.StackFactories {
+					dict[stackFactory.Name()] = stackFactory
+				}
+				var errList error = nil
+				for _, stackName := range factory.StackNames {
+					if _, ok := dict[stackName]; !ok {
+						s := fmt.Sprintf("stack definition %v, not found", stackName)
+						errList = multierr.Append(errList, fmt.Errorf(s))
 					}
 				}
-				if factory != nil {
-					dict := make(map[string]common.IStackDefinition)
-					for _, stackFactory := range params.StackFactories {
-						dict[stackFactory.Name()] = stackFactory
-					}
-					var errList error = nil
-					for _, stackName := range factory.StackNames {
-						if _, ok := dict[stackName]; !ok {
-							s := fmt.Sprintf("stack definition %v, not found", stackName)
-							errList = multierr.Append(errList, fmt.Errorf(s))
-						}
-					}
-					if errList != nil {
-						params.CancelFunc()
-						params.ConnectionCancelFunc("On stack creation", false, errList)
-						return nil, nil, nil, errList
-					}
+				if errList != nil {
+					params.CancelFunc()
+					params.ConnectionCancelFunc("On stack creation", false, errList)
+					return CreateStackDefinition{}, errList
+				}
 
-					inboundPipeDefinition := common.NewInboundPipeDefinition(
-						func() []common.IInboundData {
-							var result []common.IInboundData
-							for _, stackName := range factory.StackNames {
-								if item, ok := dict[stackName]; ok {
-									result = append(result, item)
-								}
-							}
-							return result
-						}())
-
-					outboundPipeDefinition := common.NewOutboundPipeDefinition(
-						func() []common.IOutboundData {
-							var result []common.IOutboundData
-							for _, stackName := range factory.StackNames {
-								if item, ok := dict[stackName]; ok {
-									result = append(result, item)
-								}
-							}
-							return result
-						}())
-
-					stackState, err := func() ([]common.IStackState, error) {
-						var allStackState []common.IStackState
-						var stacks []common.IStackDefinition
+				inboundPipeDefinition := common.NewInboundPipeDefinition(
+					func() []common.IInboundData {
+						var result []common.IInboundData
 						for _, stackName := range factory.StackNames {
 							if item, ok := dict[stackName]; ok {
-								stacks = append(stacks, item)
+								result = append(result, item)
 							}
 						}
-						for _, item := range stacks {
-							stackState := item.StackState()
-							if stackState == nil {
-								continue
-							}
-							b := true
-							b = b && (0 != strings.Compare("", stackState.GetId()))
-							b = b && (stackState.OnCreate() != nil)
-							b = b && (stackState.OnDestroy() != nil)
-							b = b && (stackState.OnStart() != nil)
-							b = b && (stackState.OnStop() != nil)
-							if !b {
-								return nil, fmt.Errorf("stackstate must be complete in full")
-							}
-							allStackState = append(allStackState, stackState)
-						}
-						return allStackState, nil
-					}()
-					if err != nil {
-						return nil, nil, nil, err
-					}
+						return result
+					}())
 
-					return stackState, inboundPipeDefinition, outboundPipeDefinition, nil
+				outboundPipeDefinition := common.NewOutboundPipeDefinition(
+					func() []common.IOutboundData {
+						var result []common.IOutboundData
+						for _, stackName := range factory.StackNames {
+							if item, ok := dict[stackName]; ok {
+								result = append(result, item)
+							}
+						}
+						return result
+					}())
+
+				stackState, err := func() ([]common.IStackState, error) {
+					var allStackState []common.IStackState
+					var stacks []common.IStackDefinition
+					for _, stackName := range factory.StackNames {
+						if item, ok := dict[stackName]; ok {
+							stacks = append(stacks, item)
+						}
+					}
+					for _, item := range stacks {
+						stackState := item.StackState()
+						if stackState == nil {
+							continue
+						}
+						b := true
+						b = b && (0 != strings.Compare("", stackState.GetId()))
+						b = b && (stackState.OnCreate() != nil)
+						b = b && (stackState.OnDestroy() != nil)
+						b = b && (stackState.OnStart() != nil)
+						b = b && (stackState.OnStop() != nil)
+						if !b {
+							return nil, fmt.Errorf("stackstate must be complete in full")
+						}
+						allStackState = append(allStackState, stackState)
+					}
+					return allStackState, nil
+				}()
+				if err != nil {
+					return CreateStackDefinition{}, err
 				}
-				return nil, nil, nil, fmt.Errorf("connectionstack factory definition \"%v\", not found", params.StackName)
-			},
+
+				return CreateStackDefinition{
+					StackState:             stackState,
+					InboundPipeDefinition:  inboundPipeDefinition,
+					OutboundPipeDefinition: outboundPipeDefinition,
+				}, nil
+			}
+			return CreateStackDefinition{}, fmt.Errorf("connectionstack factory definition \"%v\", not found", params.StackName)
 		},
 	)
 }
