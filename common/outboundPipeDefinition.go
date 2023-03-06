7b5493b98f733f767d5186089749a29de3f738ff
Brendan Bosman <bhbosman@gmail.com>
2023-03-06 10:08:04 +0200

    refactoring

--- common/outboundPipeDefinition.go
+++ common/outboundPipeDefinition.go
@@ -8,28 +8,6 @@ import (
 	"strings"
 )
 
-type IBoundData interface {
-	Name() string
-	Bound() BoundResult
-}
-
-type boundData struct {
-	name  string
-	bound BoundResult
-}
-
-func (self *boundData) Name() string {
-	return self.name
-}
-
-func (self *boundData) Bound() BoundResult {
-	return self.bound
-}
-
-func NewBoundData(name string, outbound BoundResult) IBoundData {
-	return &boundData{name: name, bound: outbound}
-}
-
 type outboundPipeDefinition struct {
 	stacks []IBoundData
 }
