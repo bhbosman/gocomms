d81d103599de6504b32210bfda5a8e44a04fffe3
Brendan Bosman <bhbosman@gmail.com>
2023-03-06 08:38:15 +0200

    refactoring

--- common/inboundPipeDefinition_test.go
+++ common/inboundPipeDefinition_test.go
@@ -3,14 +3,21 @@ package common_test
 import (
 	"github.com/bhbosman/gocomms/common"
 	"github.com/reactivex/rxgo/v2"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
 	"testing"
 )
 
 func TestCurrentBehaviorWithNoDefinedStacks(t *testing.T) {
 	def := common.NewInboundPipeDefinition(nil)
-
 	items := make(chan rxgo.Item)
+	defer close(items)
+	m := make(map[string]*common.StackDataContainer)
 
-	def.BuildIncomingObs(items)
+	obs, err := def.BuildIncomingObs(items, m, context.Background())
+	if !assert.NoError(t, err) {
+		return
+	}
+	obs.InboundObservable.Observe()
 
 }
