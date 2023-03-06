c973a8e43d1884846e888959247a1d1e1ccb0ae7
Brendan Bosman <bhbosman@gmail.com>
2023-03-06 06:59:43 +0200

    refactoring

--- common/stackState.go
+++ common/stackState.go
@@ -21,3 +21,63 @@ type StackStartState func(
 type StackStopState func(
 	stackData interface{},
 ) error
+
+type StackState struct {
+	Id          string
+	HijackStack bool
+	Create      StackCreate
+	Destroy     StackDestroy
+	Start       StackStartState
+	Stop        StackStopState
+}
+
+func (self *StackState) GetId() string {
+	return self.Id
+}
+
+func (self *StackState) GetHijackStack() bool {
+	return self.HijackStack
+}
+
+func (self *StackState) OnCreate() StackCreate {
+	return self.Create
+}
+
+func (self *StackState) OnDestroy() StackDestroy {
+	return self.Destroy
+}
+
+func (self *StackState) OnStart() StackStartState {
+	return self.Start
+}
+
+func (self *StackState) OnStop() StackStopState {
+	return self.Stop
+}
+
+type IStackState interface {
+	GetId() string
+	GetHijackStack() bool
+	OnCreate() StackCreate
+	OnDestroy() StackDestroy
+	OnStart() StackStartState
+	OnStop() StackStopState
+}
+
+func NewStackState(
+	id string,
+	hijackStack bool,
+	create StackCreate,
+	destroy StackDestroy,
+	start StackStartState,
+	stop StackStopState,
+) IStackState {
+	return &StackState{
+		Id:          id,
+		HijackStack: hijackStack,
+		Create:      create,
+		Destroy:     destroy,
+		Start:       start,
+		Stop:        stop,
+	}
+}
