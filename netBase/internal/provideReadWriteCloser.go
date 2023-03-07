93a18168efbad581eb5f7ecd17d8613f4c745ec0
Brendan Bosman <bhbosman@gmail.com>
2023-03-07 08:47:53 +0200

    refactoring

--- netBase/internal/provideReadWriteCloser.go
+++ netBase/internal/provideReadWriteCloser.go
@@ -9,12 +9,12 @@ import (
 func ProvideReadWriteCloser(rwc io.ReadWriteCloser) fx.Option {
 	return fx.Provide(
 		fx.Annotated{
-			Name: "Primary",
-			Target: func() (io.ReadWriteCloser, error) {
+			//Name: "Primary",
+			Target: func() (io.ReadWriteCloser, io.Reader, io.Writer, io.Closer, error) {
 				if rwc == nil {
-					return nil, fmt.Errorf("connection is nil. Please resolve")
+					return nil, nil, nil, nil, fmt.Errorf("connection is nil. Please resolve")
 				}
-				return rwc, nil
+				return rwc, rwc, rwc, rwc, nil
 			},
 		},
 	)
